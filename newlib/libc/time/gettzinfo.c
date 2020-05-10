c418c6b94269842804382f5c953ace2a3ad36860
Denis Feklushkin <denis.feklushkin@gmail.com>
2020-05-10 14:28:21 +0700

    backup

--- newlib/libc/time/gettzinfo.c
+++ newlib/libc/time/gettzinfo.c
@@ -1,6 +1,7 @@
 /* Copyright (c) 2005 Jeff Johnston <jjohnstn@redhat.com> */
 #include <sys/types.h>
 #include <local.h>
+#include <time.h>
 
 /* Shared timezone information for libc/time functions.  */
 static __tzinfo_type tzinfo = {1, 0,
